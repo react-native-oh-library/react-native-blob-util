b06360f5eafcd2cbbcf3f0028bcc97b59d91e782
zWX1317523 <zwy2961718523@163.com>
2024-12-12 18:16:48 +0800

    fix: 修改版权头及publishConfig

--- harmony/blobUtil/src/main/ets/ReactNativeBlobUtil/ReactNativeBlobUtilFS.ts
+++ harmony/blobUtil/src/main/ets/ReactNativeBlobUtil/ReactNativeBlobUtilFS.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import HashMap from '@ohos.util.HashMap';
 import common from '@ohos.app.ability.common';
 import fs from '@ohos.file.fs';
--- harmony/blobUtil/src/main/ets/ReactNativeBlobUtil/ReactNativeBlobUtilReq.ts
+++ harmony/blobUtil/src/main/ets/ReactNativeBlobUtil/ReactNativeBlobUtilReq.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import http from '@ohos.net.http';
 import fs from '@ohos.file.fs';
 import buffer from '@ohos.buffer';
